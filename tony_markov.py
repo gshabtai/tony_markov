287b7dedc64f13535f261125e3bf9b95e9b98dcb
Guy Shabtai <gshabtai@ucsd.edu>
2022-07-14 15:11:27 +0200

    comments

--- tony_markov.py
+++ tony_markov.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+# Created by Guy Shabtai
+
 import random
 import sys
 from os.path import exists,isdir
 from os import listdir
 from datetime import date, datetime
 
+# A class used for the post part of mapping pre-to-post
 class Post:
     def __init__(self):
-        self.text = []
-        self.encoding = {}
+        self.text = [] # Contains all the potential posts. Quantity is used to achieve weight. Used in LOAD.
+        self.encoding = {} # Contains mapping of post to weight of post as an integer. Used in MAKE.
 
+    # Adds text to the class, in a way that adds it both to self.text and self.encoding. Used in MAKE.
     def add_text(self,s):
         self.text.append(s)
         val = self.encoding.get(s)
@@ -18,6 +22,7 @@ class Post:
         else:
             self.encoding.update({s : val+1})
 
+    # Gets a random text from possible posts, weighted.
     def get_text(self):
         if len(self.text) == 1:
             return self.text[0]
@@ -28,23 +33,23 @@ def count(s):
 
 markov = {}
 
-USAGE = "\n\ndir_name =\tdirectory containing trining files, only .txt\nPRE_LENGTH =\tlength of markov state in words\nPOST_LENGTH =\tlength of markov outputs in words\nfile_name =\tname of generated file, only .swag\n\nUsage:\ttony_markov.py make <dir_name> <PRE_LENGTH> <POST_LENGTH>\n\ttony_markov.py load <file_name>"
-if len(sys.argv) < 2:
+# Verifies input
+USAGE = "Usage\n\ndir_name =\tdirectory containing trining files, only .txt\nPRE_LENGTH =\tlength of markov state in words\nPOST_LENGTH =\tlength of markov outputs in words\nfile_name =\tname of generated file, only .swag\n\nUsage:\ttony_markov.py make <dir_name> <PRE_LENGTH> <POST_LENGTH>\n\ttony_markov.py load <file_name>"
+if len(sys.argv) < 2 or sys.argv[1].upper() == "HELP":
     raise Exception(USAGE)
 elif sys.argv[1].upper() not in ("MAKE","LOAD"):
-    raise Exception(USAGE)
+    raise Exception("Unrecognized command \"%s\"" % sys.argv[1].upper())
 elif sys.argv[1].upper() == "MAKE" and len(sys.argv) != 5:
-    raise Exception(USAGE)
+    raise Exception("Wrong number of arguments for load (expected 3)")
 elif sys.argv[1].upper() == "MAKE" and (int(sys.argv[3]) <= 0 or int(sys.argv[4]) <= 0):
-    raise Exception(USAGE)
+    raise Exception("Pre and post arguments must be greater than 0")
 elif sys.argv[1].upper() == "LOAD" and len(sys.argv) != 3:
-    raise Exception(USAGE)
+    raise Exception("Wrong number of arguments for load (expected 1)")
 elif not exists(sys.argv[2]) and sys.argv[1].upper() == "LOAD":
     raise Exception("Can't find file \"%s\"" % sys.argv[2])
 elif not isdir(sys.argv[2]) and sys.argv[1].upper() == "MAKE":
     raise Exception("Can't find directory \"%s\"" % sys.argv[2])
 else:
-
     if sys.argv[1].upper() == "MAKE":
         PRE_LENGTH = int(sys.argv[3])
         operation = "MAKE"
@@ -54,31 +59,37 @@ else:
         operation = "LOAD"
         print("Doing load...")
 
-    
-
+    # The MAKE operation
     if operation == "MAKE":
-        time = date.today().strftime("%d%m%Y") + datetime.now().strftime("%H%M%S")
+        time = date.today().strftime("%d%m%Y") + datetime.now().strftime("%H%M%S") # Records time used for formatting file name
         fw = open("markov_"+time+".swag","x")
+
+        # Iterates over files in the given directory.
         for f_name in listdir(sys.argv[2]):
             if not f_name.endswith(".txt"):
                 continue
 
             fs = open(f_name,"r")
+
+            # Reads through the whole file
             while True:
                 line = fs.readline()
                 if not line:
                     break
 
+                # Gets the current line, ignoring ' signs
                 new_line = ""
                 for c in line.lower():
                     if c != '\'':
                         new_line += c
                 line = new_line
 
+                # Ignores empty lines
                 line_arr = line.strip().split()
                 if len(line_arr) == 0:
                     continue
 
+                
                 pre = ""
                 post = ""
                 init_string = ""
@@ -86,6 +97,7 @@ else:
                 for word in line_arr:
                     word = word.strip()
                     
+                    # Makes sure init_string has enough words to fill 1 pre and 1 post.
                     if count(init_string) < PRE_LENGTH + POST_LENGTH:
                         init_string = init_string + word + " "
 
@@ -94,10 +106,13 @@ else:
                             initializing = False
                             init_string_arr = init_string.strip().split()
                             idx = 0
+
+                            # fills pre from init_string
                             for i in range(PRE_LENGTH):
                                 pre = pre + init_string_arr[idx] + " "
                                 idx += 1
 
+                            # fills post for init_string
                             for i in range(POST_LENGTH):
                                 post = post + init_string_arr[idx] + " "
                                 idx += 1
@@ -105,12 +120,13 @@ else:
                             pre = pre.strip()
                             post = post.strip()
 
-                        # update correctly
+                        # Updates markov mapping of pre-to-post
                         curr_post = markov.get(pre)
                         if curr_post is None:
                             markov.update({pre : Post()})
                         markov.get(pre).add_text(post)
 
+                        # Finds the next pre-post pair.
                         if PRE_LENGTH == 1 and POST_LENGTH == 1:
                             pre = post
                             post = word
@@ -124,7 +140,7 @@ else:
                             pre = pre.split(' ',1)[1] + " " + post.split()[0]
                             post = post.split(' ',1)[1] + " " + word
 
-                # update correctly, for the final word of line
+                # Update markov mapping of pre-to-post, for the final word of line.
                 curr_post = markov.get(pre)
                 if curr_post is None:
                     markov.update({pre : Post()})
@@ -132,6 +148,7 @@ else:
 
             fs.close()
 
+        # Calculates multiplicity
         num_items = 0
         total = 0
         for item in markov.items():
@@ -139,6 +156,7 @@ else:
             num_items += 1
         ratio = str(total/num_items)
 
+        # Writes file
         fw.write(ratio+ "\n" + str(PRE_LENGTH) + '\n' + str(POST_LENGTH) + "\n")
         for item in markov.items():
             fw.write(item[0])
@@ -153,16 +171,19 @@ else:
 
         print("Done! %s\nUse \"python tony_markov.py load %s\"" % (mult_txt,("markov_"+time+".swag")))
 
+    # The LOAD operation
     else:
         fs = open(sys.argv[2],"r")
         line = fs.readline()
         if not line:
             raise Exception("File bad!")
 
+        # Reads multiplicity from file.
         line = line.strip()
         mult_txt = "Multiplicity: %s. Recommended to be at least 2." % line
         print(mult_txt)
 
+        # Reads pre and post lengths from file.
         for i in range(2):
             line = fs.readline()
             line = line.strip()
@@ -178,6 +199,7 @@ else:
 
             line = line.strip().split()
 
+            # Reads pre from file
             new_pre = ""
             for i in range(PRE_LENGTH):
                 new_pre = new_pre + line[i] + " "
@@ -188,6 +210,7 @@ else:
 
             idx1=0
 
+            # Reads all possible posts for the pre in this line, and adds to Post() object.
             for i in range(PRE_LENGTH,len(line)):
                 if idx1 < POST_LENGTH:
                     post_txt = post_txt + line[i] + " "
@@ -196,8 +219,7 @@ else:
                     for j in range(int(line[i])):
                         new_post.text.append(post_txt[:-1])
                     post_txt = ""
-                    idx1=0
-                    
+                    idx1=0  
             markov.update({new_pre : new_post})
                   
         fs.close()
@@ -206,6 +228,7 @@ else:
         #for item in markov.items():
         #    print(item[0],item[1].text)
 
+        # Determines approximately how many words will be outputed.
         times = 0
         while times <= 0:
             times = int(input("Done! Make how many words? ")) // (POST_LENGTH)
@@ -213,6 +236,7 @@ else:
         #alternate use: get a random pre to start with
         #s = list(markov)[random.randint(0,len(markov.keys())-1)]
 
+        # Get a pre to start with from this list
         starts = [
             "how you guys doing this is very important in reference",
             "how you guys doing this is another thing of importance in reference"
@@ -220,8 +244,8 @@ else:
 
         s = starts[random.randint(0,len(starts)-1)]
         
-        total = 0
-        added = 0
+        total = 0 # Used only to determine chain effectiveness. Effectivesness is how many times the chain is able to use a pre-post pair to determine the next word, instead of a random pre.
+        added = 0 # Used only to determine chain effectiveness.
         print()
         for i in range(times):
             total += 1
@@ -231,9 +255,12 @@ else:
                 key = key + temp[i] + " "
             key = key.strip()
             to_add = markov.get(key)
+
+            # If we found a post for the current pre, add it.
             if to_add is not None:
                 s = s + " " + to_add.get_text()
                 added += 1
+            # Otherwise, just continue with a random pre.
             else:
                 s = s + " " + list(markov)[random.randint(0,len(markov.keys())-1)]
         print(s)
